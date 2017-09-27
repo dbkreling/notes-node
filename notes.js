893c5de2fb1046a4139949c468f652b8261d66fe
Daniel Kreling <dbkreling@gmail.com>
2017-09-27 07:23:40 -0300

    notes.js: refactor and improve addNote function
    This commit refactors addNote function, abstracting some code to get it used
    in some other functions as well.
    It also improves the output of the 'add' command.
    
    Ref.: Lecture 17
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- notes.js
+++ notes.js
@@ -2,26 +2,32 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+    // Load the notes-data file into an array. Treat exception if ENOENT
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
-
-    // Load the notes-data file into an array. Treat exception if ENOENT
-    try {
-        var notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (e) {
-
-    }
-
     // Verify for duplicate notes. If no duplicates, write to file.
     var duplicateNotes = notes.filter((note) => note.title === title);
     if (duplicateNotes.length == 0) {
         notes.push(note);
-        fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
 };
 
