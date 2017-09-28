2e40349f0da3a2ca470ba18a76f43d82f75a6fe0
Daniel Kreling <dbkreling@gmail.com>
2017-09-28 07:31:19 -0300

    notes.js: implement read_note function
    Implement get_note() on notes.js to return the selected note passed on from
    the CLI. The commit also modifies app.js to behave accordingly and display
    the found note to the user.
    
    Ref.: Lecture 19
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ console.log('Yargs', argv);
 if (command == 'add') {
     var note = notes.addNote(argv.title, argv.body);
     if (note) {
-        console.log('---\nNote created: ');
+        console.log('Note created');
+        console.log('---');
         console.log(`Title: ${note.title}`);
         console.log(`Body: ${note.body}`);
     } else {
@@ -23,7 +24,16 @@ if (command == 'add') {
 } else if (command == 'list') {
     notes.getAll();
 } else if (command == 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        console.log('---');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Note not found');
+    }
+
 } else if (command == 'remove') {
     var noteRemoved = notes.removeNote(argv.title);
     var message = noteRemoved ? 'Note was removed' : 'Note not found';
